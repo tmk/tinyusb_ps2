addabfb09e969f83d90e941b62198ba66d45ee0f
Jun Wako <wakojun@gmail.com>
2022-12-15 20:40:02 +0900

    Fix Device descriptor for IAD
    https://www.usb.org/sites/default/files/iadclasscode_r10.pdf

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -47,9 +47,9 @@ tusb_desc_device_t const desc_device =
     .bLength            = sizeof(tusb_desc_device_t),
     .bDescriptorType    = TUSB_DESC_DEVICE,
     .bcdUSB             = USB_BCD,
-    .bDeviceClass       = 0x00,
-    .bDeviceSubClass    = 0x00,
-    .bDeviceProtocol    = 0x00,
+    .bDeviceClass       = TUSB_CLASS_MISC,
+    .bDeviceSubClass    = MISC_SUBCLASS_COMMON,
+    .bDeviceProtocol    = MISC_PROTOCOL_IAD,
     .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
 
     .idVendor           = USB_VID,
